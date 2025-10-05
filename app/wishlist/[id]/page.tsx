af33954823a3e0bb983ab7d28088cb8e86d24e88
Rotaienko Oleksandr <sasharotaenko1@gmail.com>
2025-10-05 23:00:33 +0300

    fix generateMetadata function

--- app/wishlist/[id]/page.tsx
+++ app/wishlist/[id]/page.tsx
@@ -5,6 +5,41 @@ import Wishlist from "@/models/Wishlist";
 
 export const dynamic = "force-dynamic";
 
+export async function generateMetadata({ params }: { params: { id: string } }) {
+    await connectMongo();
+    interface WishlistLean {
+        title: string;
+      }
+      
+      const wishlist = await Wishlist.findById(params.id).lean<WishlistLean>();
+      const title = wishlist?.title || "Wishlist on Wishlify";
+      
+  
+    return {
+      title,
+      description: "A wishlist created with ❤️ on Wishlify",
+      openGraph: {
+        title,
+        description: "Check out this wishlist on Wishlify",
+        url: `https://wishlify.me/wishlist/${params.id}`,
+        images: [
+          {
+            url: `https://wishlify.me/api/og/wishlist/${params.id}`,
+            width: 1200,
+            height: 630,
+            alt: title,
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title,
+        description: "Check out this wishlist on Wishlify",
+        images: [`https://wishlify.me/api/og/wishlist/${params.id}`],
+      },
+    };
+  }
+  
 
 export default async function WishlistPage({ params }: { params: { id: string } }) {
   await connectMongo();
