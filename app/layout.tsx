5bab6e8a88c6283893074ce21f1e058cac106946
Rotaienko Oleksandr <sasharotaenko1@gmail.com>
2026-01-18 22:36:54 +0200

    add predly code

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,19 @@ export const metadata = getSEOTags();
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" data-theme={config.colors.theme} className={font.className}>
+      <!-- BEGIN PLERDY CODE -->
+<script type="text/javascript" defer data-plerdy_code='1'>
+    var _protocol="https:"==document.location.protocol?"https://":"http://";
+    _site_hash_code = "1d7fe47d78d3dc90802bab2a811c4a5f",_suid=72066, plerdyScript=document.createElement("script");
+    plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
+    plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
+    var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
+    plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
+    try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
+</script>
+<!-- END PLERDY CODE -->
+
+
 <Analytics/>      <body>
         <PlausibleProvider domain={config.domainName} trackOutboundLinks>{/* optional props */}
           {/* ClientLayout wraps client-only services (chat, toasts, tooltips, etc.) */}
